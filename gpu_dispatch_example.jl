cdcf9de1913bc9cb270144da523e75e736b06d51
Frederik Scheiderbauer <frederik.scheiderbauer@gmx.de>
2021-07-12 11:16:27 +0200

    config file experiment

--- gpu_dispatch_example.jl
+++ gpu_dispatch_example.jl
@@ -1,11 +1,14 @@
-import Pkg; 
-Pkg.add("BenchmarkTools")
-Pkg.add("CUDA")
 
 using CUDA;
+using JSON;
+
+dicttxt = JSON.parsefile("config_example.json")
+
+x2 = dicttxt["x"]
+y2 = dicttxt["y"]
 N = 256*2
-x_d = CUDA.fill(1.0, N)  # a vector stored on the GPU filled with 1.0 (Float32)
-y_d = CUDA.fill(2.0, N)  # a vector stored on the GPU filled with 2.0
+x_d = CUDA.fill(convert(Float64,x2), N)  # a vector stored on the GPU filled with 1.0 (Float32)
+y_d = CUDA.fill(convert(Float64,y2), N)  # a vector stored on the GPU filled with 2.0
 
 function myadd(x::Float32, y::Float32)
     return x+2*y
