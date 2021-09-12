0d8addfe1f09a1fa3fa98f35c771018c53db6b3a
Motoerdeath <frederik.scheiderbauer@gmx.de>
2021-09-12 17:00:01 +0300

    added MatrixUtils functions.on gpu implementation

--- environments/MonkeyTestingDummyApp/gym_jadx/util/Matrix_utils.jl
+++ environments/MonkeyTestingDummyApp/gym_jadx/util/Matrix_utils.jl
@@ -1,16 +1,57 @@
 using Images
-
-
-include("Path_utils.jl")
-
+using CUDA 
 
 function get_array_of_image(filename,resized = true)
-    image_path = get_image_path(filename,resized)
+    if resized
+        image_path= "../../resources/drawables/" * filename
+    else
+        image_path= "../../resources/drawables/size_original/" * filename
+    end
     img = load(image_path)
-    img_RGB = RGB.(img)
-    return permutedims(channelview(img_RGB),(3,2,1))
+    return permutedims(channelview(RGB.(img)),(3,2,1))
+end
+
+
+function includes_point(click_coordinates,absolute_coordinates,width,height)
+
+    return (absolute_coordinates[1] <= click_coordinates[1] <= (absolute_coordinates[1]+width) && absolute_coordinates[2] <= click_coordinates[2] <= (absolute_coordinates[2]+height))
+end
+function includes_point(click_coordinate_x,click_coordinate_y,absolute_coordinate_x,absolute_coordinate_y,width,height)
+
+    return (absolute_coordinate_x <= click_coordinate_x <= (absolute_coordinate_x+width) && absolute_coordinate_y <= click_coordinate_y <= (absolute_coordinate_y+height))
+end
+
+#written for cpu computation, probably needs to be changed for GPU
+function __blit_single_channel_inplace(dest,src,x,y)
+    dest[x:x+size(src,1)-1,y:y+size(src,2)-1] = src
+end
+
+#number threads needs to be bigger than number of rows of src
+#TODO make work for src images with more rows than threadnumber
+function kernel_blit_single_channel_inplace(threadID,dest,src,x,y)
+    if threadID <= size(src,1)
+        for i in 1:size(src,2)
+            @inbounds dest[x-1+threadID,y-1+i] = src[threadID,i]
+        end
+    end
+    return
 end
 
 
 
-get_array_of_image("menu_über_unclicked.png")
+function kernel_blit_image_inplace(dest,src,x,y)
+    tx = threadIdx().x
+    kernel_blit_single_channel_inplace(tx,dest[:,:,1], src[:,:,1],x,y)
+    kernel_blit_single_channel_inplace(tx,dest[:,:,2], src[:,:,2],x,y)
+    kernel_blit_single_channel_inplace(tx,dest[:,:,3], src[:,:,3],x,y)
+    return
+end
+
+a = CUDA.fill(0,(50,50,3))
+b = CUDA.fill(1,(10,10,3))
+x = 10
+y = 10
+display(a)
+#@cuda threads=10 kernel_blit_image_inplace(a,b,x,y)
+CUDA.synchronize()
+#display(a)
