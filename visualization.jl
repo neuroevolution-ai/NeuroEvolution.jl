ca3d083798f14abba7059d3650d83a3d4f49fbaa
Julien Aziz <@julien.aziz>
2022-01-18 10:40:44 +0100

    adjusted visualization script

--- visualization.jl
+++ visualization.jl
@@ -12,13 +12,13 @@ function kernel_test_initialize(environments, env_seed)
     offset = sizeof(input)
 
     sync_threads()
-
-    initialize(tx, bx, input, environments, offset, env_seed)
+    
+    initialize(tx, bx, environments, offset, env_seed)
 
     return
 end
 
-function kernel_test_collect_points(action, environments, env_seed)
+function kernel_test_collect_points(action, observations, environments, env_seed)
 
     tx = threadIdx().x
     bx = blockIdx().x
@@ -26,7 +26,7 @@ function kernel_test_collect_points(action, environments, env_seed)
     input = @cuDynamicSharedMem(Float32, environments.number_inputs)
     offset = sizeof(input)
     sync_threads()
-    step(tx, bx, action, offset, environments)
+    step(tx, bx, action, observations, offset, environments)
 
     return
 end
@@ -47,7 +47,6 @@ function main()
     config_environment["number_time_steps"] = 1000
 
     env_seed = rand(1:1000)
-    number_individuals = 100
 
     environments = CollectPoints(config_environment, number_individuals)
 
@@ -61,14 +60,16 @@ function main()
     width = environments.maze_rows * environments.maze_cell_size
     height = environments.maze_columns * environments.maze_cell_size
 
+    observations = CUDA.fill(0.0f0, (10, number_individuals))
+
     @play width height number_iterations begin
 
         action = CuArray(rand(Uniform(-1.0, 1.0), 2))
-        CUDA.@cuda threads = 10 blocks = number_individuals shmem = shared_memory kernel_test_collect_points(action, environments, env_seed)
+        CUDA.@cuda threads = 10 blocks = number_individuals shmem = shared_memory kernel_test_collect_points(action, observations, environments, env_seed)
 
         CUDA.synchronize()
 
-        render(environments)
+        render(environments, 1)
     end
 end
 
