7e8bf01aff45d74f925d142bd50788334428acf6
Daniel Zimmermann <dzimmer@fzi.de>
2022-03-18 15:30:21 +0100

    Modified rng test

--- test/rng_test.jl
+++ test/rng_test.jl
@@ -1,30 +1,31 @@
 using Plots
+using Test
 
 include("../tools/linear_congruential_generator.jl")
 
 function main()
-    seed = 5
+    seed = 6
 
-    number_randoms = 10
+    number_randoms = 10000
 
     lower_bound = 0
 
-    upper_bound = 5
+    upper_bound = 20
 
-    states = fill(seed, 10)
+    states = fill(seed, 20)
 
     randoms = fill(0, number_randoms)
     randoms_compare = fill(0, number_randoms)
 
     for i = 1:number_randoms
         seed, randoms[i] = lgc_random(seed, lower_bound, upper_bound)
-        randoms_compare[i] = lgc_random(states, 1, 0, 5)
+        randoms_compare[i] = lgc_random(states, 1, lower_bound, upper_bound)
     end
     
     gr()
-    display(histogram(randoms, bins=(upper_bound-lower_bound + 1), alpha=0.5, label="5"))
+    display(histogram(randoms, bins=(upper_bound-lower_bound + 1), alpha=0.5))
 
-    println(randoms == randoms_compare)
+    @test randoms == randoms_compare
 end
 
 main()
