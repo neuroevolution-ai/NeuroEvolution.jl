a09ea9651213f0a317102d8efed211253777d98b
Daniel Zimmermann <dzimmer@fzi.de>
2022-10-25 15:20:12 +0200

    Added tests to runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,14 @@ using Test
 @testset "Summary" begin
 
 include("cma_es.jl")
-include("collect_points.jl")
+
 include("continuous_time_rnn.jl")
 include("feed_forward_nn.jl")
+include("gated_recurrent_unit_nn.jl")
+include("long_short_term_memory_nn.jl")
+include("elman_network.jl")
+
+include("collect_points.jl")
 include("dummy_app.jl")
 
 end
